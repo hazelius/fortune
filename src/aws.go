98d7be57b8fe75bfb364faf10ae1e341e5909334
Yosuke Hasegawa <yosuke.hasegawa@cydas.com>
2019-08-20 13:14:26 +0900

    ファイル閉じ忘れ

--- src/aws.go
+++ src/aws.go
@@ -5,16 +5,16 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func InitAws() *session.Session {
 	sess, err := session.NewSession(&aws.Config{
-    Region:      aws.String("ap-northeast-1"),
-    Credentials: credentials.NewStaticCredentials(GetAwsInfo("access_key"), GetAwsInfo("secret_key"), ""),
+		Region:      aws.String("ap-northeast-1"),
+		Credentials: credentials.NewStaticCredentials(GetAwsInfo("access_key"), GetAwsInfo("secret_key"), ""),
 	})
 	if err != nil {
 		fmt.Println(err.Error())
@@ -26,16 +26,16 @@ func ShowList() {
 	client := s3.New(InitAws())
 
 	input := &s3.ListObjectsV2Input{
-    Bucket:  aws.String(GetAwsInfo("s3_bucket_dl")),
+		Bucket: aws.String(GetAwsInfo("s3_bucket_dl")),
 	}
 
 	resp, err := client.ListObjectsV2(input)
-  if err != nil {
-    panic(err)
-  }
-  for _, c := range resp.Contents {
-    fmt.Println(*c.Key)
-  }
+	if err != nil {
+		panic(err)
+	}
+	for _, c := range resp.Contents {
+		fmt.Println(*c.Key)
+	}
 }
 
 func Download(object_key string, filename string) error {
@@ -45,16 +45,17 @@ func Download(object_key string, filename string) error {
 	// Create a file to write the S3 Object contents to.
 	f, err := os.Create(filename)
 	if err != nil {
-	    return fmt.Errorf("failed to create file %q, %v", filename, err)
+		return fmt.Errorf("failed to create file %q, %v", filename, err)
 	}
+	defer f.Close()
 
 	// Write the contents of S3 Object to the file
 	n, err := downloader.Download(f, &s3.GetObjectInput{
-	    Bucket: aws.String(GetAwsInfo("s3_bucket_dl")),
-	    Key:    aws.String(object_key+"/"+filename),
+		Bucket: aws.String(GetAwsInfo("s3_bucket_dl")),
+		Key:    aws.String(object_key + "/" + filename),
 	})
 	if err != nil {
-	    return fmt.Errorf("failed to download file, %v", err)
+		return fmt.Errorf("failed to download file, %v", err)
 	}
 	fmt.Printf("file downloaded, %d bytes\n", n)
 
