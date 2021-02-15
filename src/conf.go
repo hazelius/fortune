87fe7055a7ab89ae89c706632115a622f9cf4be6
Yosuke Hasegawa <yosuke.hasegawa@cydas.com>
2021-02-15 12:57:33 +0900

    viperを削除

--- src/conf.go
+++ src/conf.go
@@ -1,28 +1,26 @@
 package main
 
-import (
-	"github.com/spf13/viper"
-)
+// var v_db = InitConf("database")
+// var v_aws = InitConf("aws")
 
-var v_db = InitConf("database")
-var v_aws = InitConf("aws")
+// func InitConf(file_name string) *viper.Viper {
+// 	v := viper.New()
+// 	v.SetConfigName(file_name)
+// 	v.AddConfigPath(".")
+// 	err := v.ReadInConfig()
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-func InitConf(file_name string) *viper.Viper {
-	v := viper.New()
-	v.SetConfigName(file_name)
-	v.AddConfigPath(".")
-	err := v.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
+// 	return v
+// }
 
-	return v
+func GetDbInfo(db_no string) map[string]string {
+	// return v_db.GetStringMapString(db_no)
+	return make(map[string]string)
 }
 
-func GetDbInfo(db_no string) map[string]string{
-	return v_db.GetStringMapString(db_no)
-}
-
-func GetAwsInfo(key string) string{
-	return v_aws.GetString(key)
+func GetAwsInfo(key string) string {
+	// return v_aws.GetString(key)
+	return ""
 }
