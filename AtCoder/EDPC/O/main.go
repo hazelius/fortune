de57ccbd8ac9ab5ce0fd0bf53fb23ad6f539349d
Yosuke Hasegawa <yosuke.hasegawa@cydas.com>
2021-04-22 01:50:12 +0900

    EDPC O

--- AtCoder/EDPC/O/main.go
+++ AtCoder/EDPC/O/main.go
@@ -21,21 +21,32 @@ func readInt() int {
 
 func run(n int, as [][]int) int {
 	dp := make([][]int, n+1)
-	dp[0] = make([]int, 1<<n)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, 1<<n)
+	}
 	dp[0][0] = 1
-	for i := 0; i < n; i++ {
-		dp[i+1] = make([]int, 1<<n)
-		for s := 0; s < 1<<n; s++ {
-			for j := 0; j < n; j++ {
-				if (s>>j)&1 == 1 && as[i][j] == 1 {
-					dp[i+1][s] = (dp[i+1][s] + dp[i][s^(1<<j)]) % mod
-				}
+	for s := 1; s < 1<<n; s++ {
+		i := bitcount(s) - 1
+		for j := 0; j < n; j++ {
+			if (s>>j)&1 == 0 || as[i][j] == 0 {
+				continue
 			}
+			dp[i+1][s] = (dp[i+1][s] + dp[i][s^(1<<j)]) % mod
 		}
 	}
 
 	// fmt.Println(dp)
 	return dp[n][(1<<n)-1]
+}
+
+func bitcount(s int) int {
+	cnt := 0
+	for i := 0; i < 21; i++ {
+		if s>>i&1 == 1 {
+			cnt++
+		}
+	}
+	return cnt
 }
 
 func main() {
