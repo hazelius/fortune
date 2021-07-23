047076e4c4a3249821095985ef21f302dbc78e5d
Yosuke Hasegawa <yosuke.hasegawa@cydas.com>
2021-07-23 14:17:57 +0900

    ABC181 C

--- AtCoder/ABC191/C/main.go
+++ AtCoder/ABC191/C/main.go
@@ -1,32 +1,62 @@
+// C - Digital Graffiti
+// https://atcoder.jp/contests/abc191/tasks/abc191_c
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
-	"strings"
+	"strconv"
 )
 
-var sc = bufio.NewScanner(os.Stdin)
+var sc *bufio.Scanner
 
-func run(x int, a []int) string {
-	result := make([]int, 0, len(a))
-	for _, v := range a {
-		if v != x {
-			result = append(result, v)
-		}
-	}
-	re := fmt.Sprintf("%v", result)
-	return re[1 : len(re)-1]
+func readInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
 }
-
-func nextLine() string {
+func readString() string {
 	sc.Scan()
 	return sc.Text()
 }
 
+func run(r io.Reader) int {
+	sc = bufio.NewScanner(r)
+	sc.Split(bufio.ScanWords)
+
+	h, w := readInt(), readInt()
+	ss := make([]string, h)
+	for i := range ss {
+		ss[i] = readString()
+	}
+
+	ans := 0
+	for i := 0; i < h-1; i++ {
+		for j := 0; j < w-1; j++ {
+			cnt := 0
+			if ss[i][j] == '#' {
+				cnt++
+			}
+			if ss[i][j+1] == '#' {
+				cnt++
+			}
+			if ss[i+1][j] == '#' {
+				cnt++
+			}
+			if ss[i+1][j+1] == '#' {
+				cnt++
+			}
+			if cnt == 1 || cnt == 3 {
+				ans++
+			}
+		}
+	}
+
+	return ans
+}
+
 func main() {
-	a := nextLine()
-	b := strings.Split(a, " ")
-	fmt.Println(b)
+	fmt.Println(run(os.Stdin))
 }
