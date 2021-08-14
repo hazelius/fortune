7b6b6e6ae0b9f16f3c3a2425fcc9dbde41a04206
Yosuke Hasegawa <yosuke.hasegawa@cydas.com>
2021-08-14 23:12:36 +0900

    ABC213 C初回回答

--- AtCoder/ABC213/C/main.go
+++ AtCoder/ABC213/C/main.go
@@ -13,15 +13,28 @@ import (
 var sc *bufio.Scanner
 
 type card struct {
-	i   int
-	val int
+	i int
+	a int
+	b int
 }
 
-type SortBy []card
+type SortByA []card
 
-func (a SortBy) Len() int           { return len(a) }
-func (a SortBy) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a SortBy) Less(i, j int) bool { return a[i].val < a[j].val }
+func (a SortByA) Len() int           { return len(a) }
+func (a SortByA) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortByA) Less(i, j int) bool { return a[i].a < a[j].a }
+
+type SortByB []card
+
+func (a SortByB) Len() int           { return len(a) }
+func (a SortByB) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortByB) Less(i, j int) bool { return a[i].b < a[j].b }
+
+type SortByI []card
+
+func (a SortByI) Len() int           { return len(a) }
+func (a SortByI) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortByI) Less(i, j int) bool { return a[i].i < a[j].i }
 
 func readInt() int {
 	sc.Scan()
@@ -34,36 +47,35 @@ func run(r io.Reader) string {
 	sc.Split(bufio.ScanWords)
 
 	_, _, n := readInt(), readInt(), readInt()
-	as, bs := make([]card, n), make([]card, n)
-	for i := range as {
-		as[i] = card{i: i, val: readInt()}
-		bs[i] = card{i: i, val: readInt()}
+	cards := make([]card, n)
+	for i := range cards {
+		cards[i] = card{i: i, a: readInt(), b: readInt()}
 	}
 
-	sort.Sort(SortBy(as))
-	sort.Sort(SortBy(bs))
-	ansa := make([]int, n)
-	ansb := make([]int, n)
-	idxa, idxb := 1, 1
-	for i := 0; i < n; i++ {
-		a, b := as[i], bs[i]
-
-		if i > 0 {
-			if as[i-1].val != a.val {
-				idxa++
-			}
-			if bs[i-1].val != b.val {
-				idxb++
-			}
+	sort.Sort(SortByA(cards))
+	idx, pre := 0, 0
+	for i, c := range cards {
+		if c.a != pre {
+			idx++
 		}
-
-		ansa[a.i] = idxa
-		ansb[b.i] = idxb
+		pre = c.a
+		cards[i].a = idx
 	}
 
+	sort.Sort(SortByB(cards))
+	idx, pre = 0, 0
+	for i, c := range cards {
+		if c.b != pre {
+			idx++
+		}
+		pre = c.b
+		cards[i].b = idx
+	}
+
+	sort.Sort(SortByI(cards))
 	ans := make([]string, n)
-	for i := 0; i < n; i++ {
-		ans[i] = fmt.Sprintf("%v %v", ansa[i], ansb[i])
+	for i, c := range cards {
+		ans[i] = fmt.Sprintf("%v %v", c.a, c.b)
 	}
 	return strings.Join(ans, "\n")
 }
